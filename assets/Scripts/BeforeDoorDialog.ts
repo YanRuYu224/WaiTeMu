56d4de445b3cb823f9087a71ce2786961d6046f5
YanJiabao <3041662941@qq.com>
2026-05-26 21:40:05 +0800

    进门左边

--- assets/Scripts/BeforeDoorDialog.ts
+++ assets/Scripts/BeforeDoorDialog.ts
@@ -11,8 +11,37 @@ export class BeforeDoorDialog extends Component {
     @property(String)
     label_str: string = ``;
 
+    @property(Node)
+    stage_one: Node = null;
+
+    @property(Node)
+    stage_two: Node = null;
+
+    @property(Node)
+    left_node: Node = null;
+
+    @property(Node)
+    right_node: Node = null;
+
+    @property(Node)
+    left_back: Node = null;
+
+    @property(Node)
+    right_back: Node = null;
+
+    @property(Node)
+    btn_left: Node = null;
+
+    @property(Node)
+    btn_right: Node = null;
+
     protected onLoad(): void {
-        this.node.on(Node.EventType.TOUCH_END, this.onClick, this);
+        this.stage_one.on(Node.EventType.TOUCH_END, this.onClick, this);
+        this.btn_left.on(Node.EventType.TOUCH_END, this.onClickLeft, this);
+        this.btn_right.on(Node.EventType.TOUCH_END, this.onClickRight, this);
+        this.left_back.on(Node.EventType.TOUCH_END, this.onClickLeftBack, this);
+        this.right_back.on(Node.EventType.TOUCH_END, this.onClickRightBack, this);
+
         this.typeWrite(this.label, this.label_str);
     }
 
@@ -44,9 +73,34 @@ export class BeforeDoorDialog extends Component {
         }, interval);
     }
 
+    resetLabel() {
+        this.label.string = ``;
+    }
+
     async onClick() {
         await Pioneer.instance.VideoManager.playLocalVideo({ clip: await Pioneer.instance.AssetManager.loadVideo("Art", "Video/开门动画") });
-        this.node.destroy();
+        this.stage_one.destroy();
+        this.resetLabel();
+    }
+
+    onClickLeft() {
+        this.stage_two.active = false;
+        this.left_node.active = true;
+    }
+
+    onClickRight() {
+        this.stage_two.active = false;
+        this.right_node.active = true;
+    }
+
+    onClickLeftBack() {
+        this.stage_two.active = true;
+        this.left_node.active = false;
+    }
+
+    onClickRightBack() {
+        this.stage_two.active = true;
+        this.right_node.active = false;
     }
 }
 
