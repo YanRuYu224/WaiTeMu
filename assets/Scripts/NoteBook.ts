9dceb284afa7ad0acacee209a44ed18a68684f86
_YanJiaBao <1816871374@qq.com>
2026-07-16 17:34:54 +0800

    移动位置修改

--- assets/Scripts/NoteBook.ts
+++ assets/Scripts/NoteBook.ts
@@ -23,7 +23,7 @@ export class NoteBook extends Component {
         if (this.isMove) return;
 
         this.isMove = true;
-        let pos = this.node.position.x === 0 ? new Vec3(0, 1200, 0) : new Vec3(0, 0, 0);
+        let pos = this.node.position.x === 0 ? new Vec3(1200, 0, 0) : new Vec3(0, 0, 0);
         tween(this.node)
             .to(1, { position: pos })
             .call(() => {
